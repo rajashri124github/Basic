6f2838119737f11d9b559fe77422fa30429cd0e0
rajashri <rajashrivmokashe@gmail.com>
2023-01-20 15:14:36 +0530

    Added Test class

---
+++ src/com/velocity/Test.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("java");
+		list.add("rajashri");
+ 
+		System.out.println(list);
+		// TODO Auto-generated method stub
+
+	}
+
+}
